6dce42bee61de565f60d399fa93de8fc37476042
Gordon Watts <gordon@gordonandpaula.net>
2015-03-05 05:31:35 +0100

    Basic coding

---
+++ memgrind_diff/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+namespace memgrind_diff
+{
+    class Program
+    {
+        /// <summary>
+        /// Simple program to generate an excel file of differences between two memgrind files.
+        /// Best run with same program or when the programs are very similar so it can collate differences.
+        /// 
+        /// Usage: memgrind_diff <file1> <file2>
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
