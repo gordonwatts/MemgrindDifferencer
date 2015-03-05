a43fc4b567092f0009715fccdba398379fa5a10f
Gordon Watts <gordon@gordonandpaula.net>
2015-03-05 10:15:05 +0100

    Remove hex addresses so that things "make sense"

--- MemgrindDifferencingEngine/Parsing/ParseMultilineMessage.cs
+++ MemgrindDifferencingEngine/Parsing/ParseMultilineMessage.cs
@@ -56,7 +56,7 @@ namespace MemgrindDifferencingEngine.Parsing
                 else
                 {
                     // In the middle of parsing this guy
-                    _currentError.Add(line);
+                    _currentError.Add(StripLineOfValgrind(line));
                 }
             }
             else
@@ -64,8 +64,31 @@ namespace MemgrindDifferencingEngine.Parsing
                 if (line.StartsWith("==") && line.Contains(_containsText))
                 {
                     _active = true;
-                    _currentError.Add(line);
+                    _currentError.Add(StripLineOfValgrind(line));
                 }
+            }
+        }
+
+        private static Regex goodLinePart = new Regex("^==[0-9]+== (?<msg>.+)$");
+        private static Regex findHex = new Regex("0x[0-9A-F]+");
+
+        /// <summary>
+        /// Clean up the line of valgrind message. In particular, remove some of the hex info
+        /// that is bound to be different run-to-run.
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        private static string StripLineOfValgrind(string line)
+        {
+            var newline = findHex.Replace(line, "0x0");
+            var m = goodLinePart.Match(newline);
+            if (!m.Success)
+            {
+                return newline;
+            }
+            else
+            {
+                return m.Groups["msg"].Value;
             }
         }
 
